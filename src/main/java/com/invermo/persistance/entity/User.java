3a67257aa7a4434f7317075024bb922f01ef701b
pd <p.defratyka28@gmail.com>
2024-01-27 12:36:09 +0100

    login functionality

---
+++ src/main/java/com/invermo/persistance/entity/User.java
@@ -0,0 +1,5 @@
+package com.invermo.persistance.entity;
+
+
+public record User(long id, String userName, String userEmail) {
+}
---
+++ src/main/java/com/invermo/persistance/entity/UsersTableConfig.java
@@ -0,0 +1,13 @@
+package com.invermo.persistance.entity;
+
+public final class UsersTableConfig {
+
+    public static final String USER_ID_COLUMN = "user_id";
+    public static final String LOGIN_COLUMN = "login";
+    public static final String PASSWORD_COLUMN = "password";
+    public static final String USER_EMAIL_COLUMN = "user_email";
+
+    private UsersTableConfig() {
+
+    }
+}
