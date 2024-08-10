867be19a05d0ae7f0ac4a2de6b1e21348b76037f
pd <p.defratyka28@gmail.com>
2024-08-10 14:51:35 +0200

    update verison

--- application/src/main/java/com/invermo/application/Main.java
+++ application/src/main/java/com/invermo/application/Main.java
@@ -1,8 +1,6 @@
 package com.invermo.application;
 
 import com.invermo.application.gui.Views;
-import com.invermo.application.state.ApplicationState;
-import com.invermo.business.domain.User;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -14,9 +12,7 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        User user=new User(1L,"qwe","qwe");
-        ApplicationState.setUser(user);
-        FXMLLoader loader = new FXMLLoader(getClass().getResource(Views.DASHBOARD_VIEW_RESOURCE));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(Views.LOGIN_VIEW_RESOURCE));
         Parent root = loader.load();
         Scene scene = new Scene(root, 1280, 800);
         stage.setTitle("Invermo");
